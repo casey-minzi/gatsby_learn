58bc46e209129aa45ab459bc295e17c1ce50be2f
casey-minzi <cluvhope@gmail.com>
2020-07-31 17:41:12 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GOOGLE_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
